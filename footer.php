d9012db19a158e70c2c9b8e0494aa48da35edf8b
dinesh <dineshmedam123@gmail.com>
2023-01-29 16:15:24 +0530

    Changes

---
+++ footer.php
@@ -0,0 +1,106 @@
+
+
+
+
+<footer class="spacer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5">
+                    <h3>Dinesh Grand</h3>
+                    <p>Experience luxury and elegance at Dinesh Grand, offering spacious Luxury rooms, Suite rooms and a grand Banquet hall in the heart of Narasaraopet. Let us make your special occasion truly memorable.
+                    </p>
+                </div>  
+                
+                <div class="contact-us col-sm-7">
+                <h3>Contact Us</h3>
+                <p>
+                     <?php echo '📍 Location: Above Louis Phillippe Showroom, Station Road, Narasaraopet, Andhra Pradesh'; ?>
+                </p>
+                <p>
+                     <?php echo '📱 Phone: +91-94914 05839'; ?>
+                </p>
+                <p>
+                    <?php echo '📧 Email: dineshgrantnrt@gmail.com'; ?>
+                </p>
+                <div class="social">
+                    <a href="#"><i class="fa-brands fa-facebook-f" data-toggle="tooltip" data-placement="top" data-original-title="facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"  data-toggle="tooltip" data-placement="top" data-original-title="instragram"style="margin-left: 15px;"></i></a>
+                </div>
+                </div>
+                 
+                <div class="col-sm-5" id="direction">            
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3830.7598564999125!2d80.04178521453278!3d16.232780888780074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a4a81d44d4849d5%3A0x4adffbc0ca3fde06!2sHotel%20Dinesh%20Grand!5e0!3m2!1sen!2sin!4v1674986493821!5m2!1sen!2sin" width="250" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+            <!--/.row--> 
+        </div>
+        
+        <!--/.container-->    
+    
+    <!--/.footer-bottom--> 
+</footer>
+
+
+
+<a href="#home" class="toTop scroll"><i class="fa fa-angle-up"></i></a>
+
+
+
+
+<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title">title</h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->    
+</div>
+
+
+
+
+
+<script src="assets/jquery.js"></script>
+
+<!-- wow script -->
+<script src="assets/wow/wow.min.js"></script>
+
+<!-- uniform -->
+<script src="assets/uniform/js/jquery.uniform.js"></script>
+
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="assets/mobile/touchSwipe.min.js"></script>
+
+<!-- jquery mobile -->
+<script src="assets/respond/respond.js"></script>
+
+<!-- gallery -->
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+
+<!-- custom script -->
+<script src="assets/script.js"></script>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,118 @@
+<?php include 'header.php';?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+
+<script>
+function scrollToElement(elementId) {
+  var element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: 'smooth' });
+}
+$("#call-us-button").click(function() {
+        window.location.href = "tel:94914 05839";
+});
+</script>
+    
+
+
+<!-- banner -->
+<div class="banner" id="banner">    	   
+    <img src="images/photos/15.jpg"   style="display:block; margin:auto;width: auto; height: 100vh; padding: 3rem!important;" alt="slide">
+    <div class="welcome-message">
+        <div class="wrap-info">
+            <div class="information">
+                <h1  class="animated fadeInDown">Dinesh Grand</h1>
+                <p class="animated fadeInUp">Where Luxury Meets Comfort</p>
+            <button id="contact-us-btn">
+                <i class="fa-solid fa-phone"></i> Contact Us
+                <a href="tel:94914 05839" id="call-us-button" ></a>
+            </button>    
+            <button id="contact-us-btn" onclick="scrollToElement('direction')">
+                <i class="fa-solid fa-location-arrow"></i> Get Directions
+                
+            </button>             
+            </div>
+            
+            <a href="#information" class="arrow-nav scroll wowload fadeInDownBig"><i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
+</div>
+<!-- banner-->
+
+
+<!-- reservation-information -->
+<div id="information" class="spacer reserve-info ">
+<div class="container">
+<div class="row">
+<div class="col-sm-7 col-md-8">
+<div class="item active"><img src="images/photos/14.jpg" class="img-responsive" alt="slide"></div>
+</div>
+<div class="col-sm-5 col-md-4">
+<h3>Our Rooms and Banquet Hall</h3>
+    <p>Welcome to Dinesh Grand Hotel, where luxury and elegance come together to provide you with an unforgettable stay. Our hotel offers 13 spacious and well-appointed luxury and suite rooms, each designed to ensure your comfort and relaxation. Our rooms feature modern amenities, such as a flat-screen TV, high-speed internet, and comfortable bedding to ensure a restful night's sleep.</p>
+
+<p>Whether you are here for a special occasion or a business trip, our hotel's banquet hall is the perfect venue to host your events. Our grand banquet hall can accommodate up to 200 guests and can be customized to suit your specific needs, whether it's a business conference, engagement, or a birthday party.</p>
+
+<p>Located in the prime Narasaraopet area, our hotel is surrounded by an array of shops, restaurants, and entertainment options. We are just 200 meters away from the Narasaraopet railway station, making it easy for our guests to explore the city and its many attractions.</p>
+
+<p>At Dinesh Grand Hotel, we pride ourselves on providing exceptional service and attention to detail. Our friendly and professional staff is dedicated to ensuring that your stay with us is as comfortable and enjoyable as possible. We look forward to welcoming you to our hotel and making your visit to Narasaraopet a memorable one.</p>
+</div>
+</div>  
+</div>
+</div>
+<!-- reservation-information -->
+
+
+
+<!-- services -->
+<div class="spacer services wowload fadeInUp" id="gallery">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="RoomCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/8.jpg" class="img-responsive" alt="slide"></div>                
+                </div>
+                
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Luxury Rooms<a href="rooms-tariff.php" class="pull-right"></a></div>
+        </div>
+
+
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="TourCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/6.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/3.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#TourCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#TourCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Suite Rooms<a href="gallery.php" class="pull-right"></i></a></div>
+        </div>
+
+
+        <div class="col-sm-4">
+            <!-- RoomCarousel -->
+            <div id="FoodCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active "><img src="images/photos/1.jpg" class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <!-- <a class="left carousel-control" href="#FoodCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#FoodCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a> -->
+            </div>
+            <!-- RoomCarousel-->
+            <div class="caption">Banquet Hall<a href="gallery.php" class="pull-right"></a></div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- services -->
+
+
+<?php include 'footer.php';?>
